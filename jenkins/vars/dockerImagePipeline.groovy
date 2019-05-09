e8970a50a311aa53862cb3a9e29ef04615a295ae
Francesco Gualazzi <inge4pres@users.noreply.github.com>
2019-05-09 09:35:43 +0200

    refactored shell command in pipeline

--- jenkins/vars/dockerImagePipeline.groovy
+++ jenkins/vars/dockerImagePipeline.groovy
@@ -4,31 +4,25 @@ def call(applicationName){
   def totalDockerTimer = metricsFactory.timer("image", "total")
 
   stage('Checkout Repo'){
-    def checkoutImageTimer = metricsFactory.timer("image", "gitCheckout")
     deleteDir()
+    def checkoutImageTimer = metricsFactory.timer("image", "gitCheckout")
     git credentialsId: 'git-read-only', url: 'https://git.company.com/namespace/repo'
     checkoutImageTimer.stop()
   }
 
   stage('Download Artifact'){
     def artifactTimer = metricsFactory.timer("image", "artifactDownload")
-    sh """
-      curl -Lo artifact.tgz 'https://artifacts.comapny.com/search?name=${applicationName}&version=latest'
-    """
+    sh "curl -Lo artifact.tgz https://artifacts.comapny.com/search?name=${applicationName}&version=latest"
     artifactTimer.stop()
   }
 
   stage('Docker Image') {
     def dockerBuild = metricsFactory.timer("image", "dockerBuild")
-    sh """
-      docker build -t docker.company.com/${applicationName}:latest .
-    """
+    sh "docker build -t docker.company.com/${applicationName}:latest . "
     dockerBuild.stop()
 
     def dockerPush = metricsFactory.timer("image", "dockerPush")
-    sh """
-      docker push docker.company.com/${applicationName}:latest
-    """
+    sh "docker push docker.company.com/${applicationName}:latest"
     dockerPush.stop()
   }
 
